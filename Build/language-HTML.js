36b7453aa3c9eb059b614570eb0a1febe44b4901
BI4HUU <BI4HUU@yandex.ru>
2019-02-09 21:57:25 +0300

    start 0.0.1

---
+++ Build/language-HTML.js
@@ -0,0 +1,23 @@
+var language_HTML;
+
+language_HTML = {
+  slides: {
+    slide1: {
+      see: {
+        row1: {
+          tag: '<h1>',
+          text: 'Language HTML',
+          tag: '</h1>'
+        },
+        row2: {
+          html: '<h1>Language HTML</h1>'
+        }
+      },
+      time: 5000,
+      voice: 'sound.mp3'
+    }
+  }
+};
+
+// tag:  5000
+// tag_text:  5000
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var stylus = require('gulp-stylus')
+var coffeescript = require('gulp-coffeescript');
+var livereload = require('gulp-livereload')
+
+
+gulp.task('html', function(){
+	return gulp.src('Source/index.pug')
+		.pipe(pug({
+			pretty: false
+		}))
+		.pipe(gulp.dest('Build'))
+		.pipe(livereload());
+});
+
+gulp.task('css', function () {
+	return gulp.src('Source/style.styl')
+		.pipe(stylus({
+			compress: true
+		}))
+	// .pipe(prefix('last 2 versions', '>1%', 'ie 9'))
+	.pipe(gulp.dest('Build'))
+	.pipe(livereload());
+});
+
+gulp.task('coffee', function() {
+  gulp.src('Source/*.coffee')
+    .pipe(coffeescript({bare: true}))
+    .pipe(gulp.dest('Build'))
+		.pipe(livereload());
+});
+
+gulp.task('js', function(){
+	return gulp.src('Source/*.js')
+		.pipe(gulp.dest('Build'))
+		.pipe(livereload());
+});
+
+gulp.task('default', [ 'watch' ]);
+gulp.task('watch', () => {
+	// livereload({ start: true })
+	livereload.listen()
+	gulp.watch('Source/*.pug', ['html']);
+	gulp.watch('Source/*.styl', ['css']);
+	gulp.watch('Source/*.js', ['js']);
+	gulp.watch('Source/*.coffee', ['coffee']);
+});
+
+// var less = require('gulp-sass');
+// var minifyCSS = require('gulp-csso');
+// var concat = require('gulp-concat');
+// var sourcemaps = require('gulp-sourcemaps');
+// gulp.task('watch', function() {
+//   livereload.listen();
+//   gulp.watch('less/*.less', ['less']);
+// });
