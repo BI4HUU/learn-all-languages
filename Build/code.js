c17c55dc0b9d8f388ccb7813bc1bfa6cecb82e20
BI4HUU <BI4HUU@yandex.ru>
2019-05-01 13:51:49 +0300

    v 0.15

--- Build/code.js
+++ Build/code.js
@@ -1,56 +1,40 @@
-const code1 = `<!-- comment -->
-<div class= "className" > tag content </div>
-Where
-<div></div> - tag
-class="className" - attribute
-tag content - content
-<!-- --> - comment(invisible on the web site)`
 
-const code2 = `<div onclick="run(1)" class="text a">Basics</div>
-<div onclick = "run(2)" class="text a" >Practice</div>
-<div onclick="run(2)" class="text a">Test</div>
+const HTML_EN_code1 = `<div onclick="run(1)" class="text a pointer">Basics</div>
+<div onclick = "run(2)" class="text a pointer" >Practice</div>
+<div onclick="run(2)" class="text a pointer">Test</div>
 <style></style>`
-
+var language = false
+var lang_name = false
 var gEval = eval;
-// var EN_HTML = new Pizzicato.Sound('./audio/EN_HTML.wav', function () {
-// 	console.log('OK')
-// 	// ukradu.play();
-// });
-// var EN_HTML = false
-// var EN_HTML2 = false
-// var EN_HTML3 = false
 
-
-// let all_voice_test = [`EN_HTML`, `EN_HTML2`]
-// all_voice_test.forEach(el => load_voice(el))
-// var if_loaded_voice = false
-// function load_voice(name_voice) {
-// 	print(`222222222222222 ${name_voice}`)
-// 	test1(name_voice)
-// }
-// function test1(name_voice) {
-// 	gEval(`if (!${name_voice}) {loaded_voice(${name_voice})}`)
-// }
-function test2(name_voice) {
-	gEval(`var ${name_voice} = new Pizzicato.Sound('./audio/${name_voice}.mp3')`)
-}
 function load_voice(name_voice) {
-	print(`3333333333333333 ${name_voice}`)
-	test2(name_voice)
+	gEval(`if(typeof ${name_voice} == "undefined") {var ${name_voice} = new Pizzicato.Sound('./audio/${name_voice}.mp3')}`)
 }
-// function voice_download(numberSlideVoice) {
-// 	allSlides[numberNextSlide].slide.voice
-// }
-function play_voice(name_voice) {
+
+function play_voice (name_voice) {
 	gEval(`${name_voice}.play()`)
 }
 var this_voice = false
-// gEval(`this_voice = ${EN_HTML}`)
 
-// var EN_HTML = new Pizzicato.Sound('./audio/EN_HTML.wav', function () {
-// 	console.log('OK')
-// 	EN_HTML.play();
-// });
+function load_lang(lang_name_noMod) {
+    lang_name = `${lang_name_noMod}_${lang_name_mod_var}`
+    print('lang_name')
+    print(lang_name)
+    gEval(`if (typeof ${lang_name} == "undefined") {load_lang2 ("${lang_name}")}`)
+}
+
+function load_lang2 (lang_name) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            gEval(`var ${lang_name} = ${xhttp.responseText}`)
+            gEval(`run_language (${lang_name})`)
+            print ` ${lang_name}`
+        }
+    };
+    xhttp.open("GET", `${lang_name}.js`, true);
+    xhttp.send();
+}
 
 
 
