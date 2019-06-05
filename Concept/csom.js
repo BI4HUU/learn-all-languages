5194c866d5f68b24ed9f9f08ba1624ab560cb11f
BI4HUU <BI4HUU@yandex.ru>
2019-06-05 09:33:42 +0300

    v 1 CSOM

--- Concept/csom.js
+++ Concept/csom.js
@@ -1,81 +1,120 @@
-let a = 
-`Slide1:
+function SlidesParser(SlidesStr) {
+	let a = SlidesStr
+	c = a.split(/\n/)
+
+	let Slides = []
+	let х = false
+	let obj = {}
+	let strLineVar = ''
+
+	function sortGroupSlide(data) {
+		if (data[0] == 1) {
+			let doubleDot = data[1].indexOf(':')
+			obj[data[1].slice(0, doubleDot)] = data[1].slice(doubleDot+2)
+		}
+		if (data[0] > 1) {
+			for (var i = 2; i < data[0]; i++) {
+				strLineVar += `	`
+			}
+			strLineVar += data[1]
+			strLineVar += `\n`
+		}
+		if (data[0] == 0 && х) {
+			strLineVar = strLineVar.substr(0, strLineVar.length - 1)
+			obj['see'] = seeTooObj(strLineVar)
+			Slides.push(obj)
+			obj['time'] = parseInt(obj['time'])
+			obj = {}
+			strLineVar = ''
+		}
+
+		if (data[0] == 0 && !х) {х = true}
+	}
+	let i = 0
+	let ii = 1
+	for (var x = 0; x < c.length; x++) {
+		function f(n) {
+			while(c[n].slice(i, ii) == `	`){
+				i++
+				ii++
+			}
+			sortGroupSlide([i, c[n].slice(i, c[n].length)])
+			i = 0
+			ii = 1
+		}
+		f(x)
+	}
+	function seeTooObj(str) {
+		let SlideObj = []
+		let strArrAll = []
+		strArr = str.split(/\n/)
+		for (var xx = 0; xx < strArr.length; xx++) {
+			function ff(n) {
+				while(strArr[n].slice(i, ii) == `	`){
+					i++
+					ii++
+				}
+				let strArrSec = [i, strArr[n].slice(i, strArr[n].length)]
+				strArrAll.push(strArrSec)
+				i = 0
+				ii = 1
+			}
+			ff(xx)
+		}
+		for (var i3 = 0; i3 < strArrAll.length; i3++) {
+			if (strArrAll[i3][0] == 0) {
+				let doubleDot = strArrAll[i3][1].indexOf(':')
+				let strLine0 = {}
+				strLine0[strArrAll[i3][1].slice(0, doubleDot)] = strArrAll[i3][1].slice(doubleDot+2)
+
+				let arrVarLvl2 = []
+				let strLineVarLvl2 = {}
+
+				if (i3+1 < strArrAll.length) {
+					let koef = 1
+					while(strArrAll[i3+koef][0] == 1){
+						let doubleDotLvl2 = strArrAll[i3+koef][1].indexOf(':')
+						let strLineLvl2 = {}
+						strLineLvl2[strArrAll[i3+koef][1].slice(0, doubleDotLvl2)] = strArrAll[i3+koef][1].slice(doubleDotLvl2+2)
+						console.log(strLineLvl2)
+						arrVarLvl2.push(strLineLvl2)
+						if (i3+koef+1 < strArrAll.length) {
+							if (strArrAll[i3+koef+1][0] == 1) {
+								koef++
+							} else {
+								break
+							}
+						} else {
+							break
+						}
+						if (i3+koef == strArrAll.length) {break}
+					}
+					strLine0[strArrAll[i3][1].slice(0, doubleDot)] = arrVarLvl2
+				}
+				SlideObj.push(strLine0)
+
+			}
+
+		}
+		return SlideObj
+	}
+	// console.log(Slides)
+	return Slides
+}
+SlidesParser(`Slide1:
 	see:
-		key31: data31
+		key1:
+			key12: data331
+			key21: data331
+			key31: data331
+		key2:
+			key1331: data331
+			key213: data31
 	time: 4000
 	voice: HTML_RU_html
 Slide2:
 	see:
-		key231: data231
+		key4: data231
 	time: 8000
 	voice: HTML_RU_html2
-Slide3:
-	see:
-		key331: data331
-	time: 18000
-	voice: HTML_RU_html3
-Slide4:
-	see:
-		key331: data331
-	time: 18000
-	voice: HTML_RU_html3
-SlideEnd`
-
-c = a.split(/\n/)
-
-let Slides = []
-let х = false
-let obj = {}
-
-function sortGroupSlide(data) {
-	if (data[0] == 1) {
-		let doubleDot = data[1].indexOf(':')
-		obj[data[1].slice(0, doubleDot)] = data[1].slice(doubleDot+2)
-	}
-	if (data[0] > 1) {
-		obj[data[1]] = 'test'
-	}
-	if (data[0] == 0 && х) {
-		Slides.push(obj)
-		obj = {}
-	}
-
-	if (data[0] == 0 && !х) {
-		х = true
-	}
-}
-let i = 0
-let ii = 1
-for (var x = 0; x < c.length; x++) {
-	f(x)
-}
-function f(n) {
-	while(c[n].slice(i, ii) == `	`){
-		i++
-		ii++
-	}
-	sortGroupSlide([i, c[n].slice(i, c[n].length)])
-	i = 0
-	ii = 1
-}
-
-	console.log(Slides)
-
-
-
-// let numberSlide = 0
-		// numberSlide++	// gEval(`Slide${numberSlide}.push([${data[0]}, '${data[1]}'])`)
-	// console.log(c[n].slice(i, c[n].length))
-	// console.log(i)
-
-// [
-// 	{
-// 		See: ``,
-// 		voice: \`HTML_RU_html\`,
-// 		time: 4000
-// 	}, {
-// 		See: ``,
-// 		voice: \`HTML_RU_html\`,
-// 		time: 4000
-// 	}
-// ]
+SlideEnd`)
